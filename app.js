8f09e776d1733f3db04cb023da8d6880ccb342ff
Calvin3c <englandisme123@gmail.com>
2023-02-19 21:00:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let count = 0;
+
+// get the #value is id  & .btn is class in html
+const value = document.querySelector("#value"); 
+const btns = document.querySelectorAll(".btn");
+
+
+// The forEach() method calls a function for each element in an array. The forEach() method is not executed for empty elements.
+// Also use call funcations
+btns.forEach(function(btn) {
+    btn.addEventListener("click",function (event){
+       const styles = event.currentTarget.classList;
+       if (styles.contains("neg")){
+        count--
+       }else if(styles.contains("pos")){
+        count++
+       }else{
+        count=0;
+       }
+
+       if(count > 0){
+        value.style.color = "green";
+       }
+
+       if(count < 0){
+        value.style.color ="red"
+       }
+
+       if(count == 0){
+        value.style.color = "black"
+       }
+
+
+
+
+
+       value.textContent = count;
+    })
+});
+
