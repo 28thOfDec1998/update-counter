c79a02375da1b9cad0ab6b00fc3da0107cbad7a9
kelvin3c <englandisme123@gmail.com>
2023-12-06 01:37:12 +0500

    .

--- app.js
+++ app.js
@@ -56,9 +56,14 @@ function currentTime() {
      mm = (mm < 10) ? "0" + mm : mm;
      ss = (ss < 10) ? "0" + ss : ss;
       
-     let time = hh + ":" + mm + ":" + ss + " " + session;
+    //  let time = hh + ":" + mm + ":" + ss + " " + session;
   
-    document.getElementById("clock").innerText = time; 
+    // document.getElementById("clock").innerText = time; 
+    document.getElementById('hrs').innerHTML = hh;
+    document.getElementById('min').innerHTML = mm;
+    document.getElementById('sec').innerHTML = ss;
+    document.getElementById('ap').innerHTML = session
+
     let t = setTimeout(function(){ currentTime() }, 1000);
   }
   
